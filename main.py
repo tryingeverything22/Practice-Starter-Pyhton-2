780b62a1ec3e480f83d6f897ff56d2748f272b89
tryingeverything22 <kallikciahmett83@gmail.com>
2025-09-27 18:07:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#print ("Hospital")
+#print ("Data+")
+#Age = 20
+#first_name = "John"
+#last_name = "Smith"               # < E X E R C I S E >
+#new_patient = True
+
+
+#print("Age:",Age,"Years Old")
+#print("Name:",first_name)
+#print("Last Name:",last_name)
+#print("New patient:",new_patient)
+
+
+#name = input ("what is your name?")
+#age = input ("what is your age?")
+
+#print ('hello'+ name)
+#print ('your age'+age)
